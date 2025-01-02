6c0c2659a86123809a318152ca8316402f5893d6
Lin Liu <linliu59@gmail.com>
2025-01-02 11:45:18 -0800

    Dec, 2024

--- journal/statistic.sh
+++ journal/statistic.sh
@@ -1,3 +1,3 @@
 (head -75 buddhism-2019.org && cat buddhism-202*) \
-  | awk '/[0-9]{2}-[0-9]{2}/ { n += $8; m +=1; } END { print "total_count avg_per_day days years"; print n, n/m, m, m/356.0; }' \
+  | awk '/[0-9]{2}-[0-9]{2}/ { n += $8; m +=1; } END { print "total_count avg_per_day days years"; print n, n/m, m, m/365.0; }' \
   | column -t
